fed28e43e133c18a97cb44a137419771abd099ce
Timothy <timkhumpan@gmail.com>
2023-05-30 03:42:08 -0400

    init

---
+++ src/main/java/com/movies/Review.java
@@ -0,0 +1,21 @@
+package com.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class Review {
+    private ObjectId id;
+    private String body;
+    private LocalDateTime created;
+    private LocalDateTime updated;
+
+
+}
